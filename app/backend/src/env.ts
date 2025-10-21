ffb38db06051894a1500dbeed866a85ddc42065b
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-10-21 03:55:27 +0000

    add: 環境変数を扱いやすい形に

---
+++ app/backend/src/env.ts
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+class EnvError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'EnvError';
+  }
+}
+
+const zStrVar = z.string().min(1);
+const zNumVar = z.preprocess((val) => {
+  if (typeof val === 'string') {
+    const parsed = Number(val);
+    if (!isNaN(parsed)) {
+      return parsed;
+    }
+  }
+  return val;
+}, z.number());
+
+const zEnv = z.object({
+  DATABASE_HOST: zStrVar,
+  DATABASE_PORT: zNumVar,
+  DATABASE_USER: zStrVar,
+  DATABASE_PASSWORD: zStrVar,
+  DATABASE_NAME: zStrVar,
+});
+
+try {
+  zEnv.parse(process.env);
+} catch (err) {
+  if (err instanceof z.ZodError) {
+    throw new EnvError(
+      `env type is invalid\n${err.issues.map((v) => `${v.message}: env.${String(v.path[0])}`).join('\n')}`
+    );
+  }
+  throw err;
+}
+
+console.log(process.env.DATABASE_HOST);
+
+declare module 'bun' {
+  interface Env extends z.infer<typeof zEnv> {}
+}
