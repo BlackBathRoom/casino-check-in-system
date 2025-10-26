429b97f944e4aa7439e2fbb846cd4e9121738629
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-10-26 16:52:26 +0000

    remove: ルートを削除し、Headerのリンクも削除

--- app/frontend/src/components/ui/Header/index.tsx
+++ app/frontend/src/components/ui/Header/index.tsx
@@ -1,13 +1,10 @@
-import { Link } from '@tanstack/react-router';
 import ThemeToggle from '@/components/ui/ThemeToggle';
 
 const Header: React.FC = () => {
   return (
     <header className="w-full navbar px-2 md:px-10 lg:px-30 py-2 shadow-md bg-base-100">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost">
-          <h1 className="text-2xl font-bbh">THE TECH CASINO BAR</h1>
-        </Link>
+        <h1 className="text-2xl font-bbh">THE TECH CASINO BAR</h1>
       </div>
       <ThemeToggle />
     </header>
