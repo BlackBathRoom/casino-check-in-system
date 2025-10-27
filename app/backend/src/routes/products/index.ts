1f6603b17922647f521d0fe072859cf229d914b3
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-10-27 12:52:28 +0000

    add: レスポンスの型を修正

--- app/backend/src/routes/products/index.ts
+++ app/backend/src/routes/products/index.ts
@@ -3,7 +3,7 @@ import { getProducts } from '@/lib/database/products';
 
 const route = new Hono().get('/', async (c) => {
   const products = await getProducts();
-  return c.json(products);
+  return c.json({ products }, 200);
 });
 
 export default route;
